ba2f3f256835fc454a53b4cd33888ce35be47000
Emmanuel <emmanuelbrenes@pm.me>
2021-10-11 11:22:06 -0600

    Adding meaningful comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,69 @@
+//! # Blog Crate
+//!
+//! `blog` is an example code from The Rust Programming Language book (aka The Book).
+//! This particular implementation is my resolution of the 3 suggestions withing the
+//! chapter 17.3 in section: Encoding States and Behavior as Types
+//!
+
+// Base struct for Post
 pub struct Post {
     content: String,
 }
 
+// Base struct for Draft
 pub struct DraftPost {
     content: String,
 }
 
 impl Post {
+    /// Creates a new DraftPost with an empty String
+    ///
+    /// # Use
+    /// ```
+    /// let post= Post::new();
+    /// ```
     pub fn new() -> DraftPost {
         DraftPost {
             content: String::new(),
         }
     }
 
+    /// Shows the content of the Post
+    ///
+    /// # Use
+    /// ```
+    /// let post = Post::new();
+    /// // snip
+    /// post.content();
+    /// ```
     pub fn content(&self) -> &str {
         &self.content
     }
 }
 
 impl DraftPost {
+    /// Adds text to the Post's content
+    ///
+    /// # Use
+    /// ```
+    /// let post = Post::new();
+    ///
+    /// post.add_text("Example text");
+    /// ```
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
     }
 
+    /// Moves the content to Review
+    ///
+    /// # Use
+    /// ```
+    /// let post = Post::new();
+    ///
+    /// post.add_text("Example text");
+    ///
+    /// let post = post.request_review();
+    /// ```
     pub fn request_review(self) -> PendingReviewPost0 {
         PendingReviewPost0 {
             content: self.content,
@@ -31,21 +72,45 @@ impl DraftPost {
 }
 
 trait Review {
+    // Generic associated type for approval
     type T;
 
     fn approve_draft(self) -> Self::T;
     fn reject_draft(self) -> DraftPost;
 }
 
+// Base struct for first review
 pub struct PendingReviewPost0 {
     content: String,
 }
 
 impl PendingReviewPost0 {
+    /// Moves the content to the second approval
+    ///
+    /// # Use
+    /// ```
+    /// let post = Post::new();
+    ///
+    /// post.add_text("Example text");
+    ///
+    /// let post = post.request_review();
+    /// let post = post.approve();
+    /// ```
     pub fn approve(self) -> PendingReviewPost1 {
         self.approve_draft()
     }
 
+    /// Returns the content back to Draft
+    ///
+    /// # Use
+    /// ```
+    /// let post = Post::new();
+    ///
+    /// post.add_text("Example text");
+    ///
+    /// let post = post.request_review();
+    /// let post = post.reject();
+    /// ```
     pub fn reject(self) -> DraftPost {
         self.reject_draft()
     }
@@ -67,15 +132,40 @@ impl Review for PendingReviewPost0 {
     }
 }
 
+// Base struct for the second review
 pub struct PendingReviewPost1 {
     content: String,
 }
 
 impl PendingReviewPost1 {
+    /// Moves the content to a Post
+    ///
+    /// # Use
+    /// ```
+    /// let post = Post::new();
+    ///
+    /// post.add_text("Example text");
+    ///
+    /// let post = post.request_review();
+    /// let post = post.approve();
+    /// let post = post.approve();
+    /// ```
     pub fn approve(self) -> Post {
         self.approve_draft()
     }
 
+    /// Returns the content back to Draft
+    ///
+    /// # Use
+    /// ```
+    /// let post = Post::new();
+    ///
+    /// post.add_text("Example text");
+    ///
+    /// let post = post.request_review();
+    /// let post = post.approve();
+    /// let post = post.reject();
+    /// ```
     pub fn reject(self) -> DraftPost {
         self.reject_draft()
     }
--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,22 @@ fn main() {
     // Will panic as DraftPost does not have a content() method like before
     // post.content();
 
+    // Draft is send to the first revision
     let post = post.request_review();
 
-    // Two approves are needed before we can publish a post
+    // Two approves are needed before we can publish a draft
     let post = post.approve();
 
+    // post goes back to Draft
     let post = post.reject();
 
+    // Draft is send to the first revision
     let post = post.request_review();
+
+    // Draft is now fully approved and it's turned into a Post
     let post = post.approve();
     let post = post.approve();
 
+    // Post is published and now we can access the content() method
     assert_eq!("I ate salad for lunch today", post.content());
 }
